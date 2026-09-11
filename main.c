8bca32e9a951174dd6b6ab6d441711a98835cbe0
Senumi <senumitp15@gmail.com>
2026-09-11 23:42:02 +0530

    Add speciality lookup data

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
 
+const int specialtyID[4] = {1, 2, 3, 4};
+
+const char specialtyName[4][30] = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+const double consultationFee[4] = {
+    1500.00,
+    2500.00,
+    4500.00,
+    5000.00
+};
+
+const int consultationTime[4] = {
+    15,
+    20,
+    30,
+    30
+};
+
+const int dailyPatientCap[4] = {
+    30,
+    20,
+    12,
+    10
+};
+
 int main(){
 
 printf("Smart Hospital & Resource Allocation System\n");
